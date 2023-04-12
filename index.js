1874eae4038206ddec6758c657a8a3a7d609d69b
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-04-12 23:29:15 +0200

    graphql resolver

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require("express");
 const app = express(); 
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema";
+import resolvers from "./resolvers";
 
 app.get("/", (req, res) => {
     res.send("GraphQL is quite lovely!");
 });
 
-const root = { product: () => {
-    return {
-        "id": 123456, 
-        "name": "widget", 
-        "description": "A very lovely garden widget", 
-        "price": 19.99, 
-        "soldout": false 
-    } 
-}};
+const root = resolvers;
 
 app.use("/graphql", graphqlHTTP({
     schema: schema, 
--- schema.js
+++ schema.js
@@ -7,10 +7,33 @@ const schema = buildSchema(`
         description: String
         price: Float
         soldout: Boolean
+        stores: [Store]!
+    }
+
+    type Store {
+        store: String
     }
 
     type Query {
-        product: Product
+        getProducts(id: ID): Product
+
+    }
+
+    input StoreInput {
+        store: String
+    }
+
+    input ProductInput {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Boolean
+        stores: [StoreInput]!
+    } 
+
+    type Mutation {
+        createProduct(input: ProductInput): Product
     }
 `); 
 
