b745a2bc36f9a9b237386776ab0f77d43af3efd8
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-04-14 12:22:42 +0200

    query all products

--- data/resolvers.js
+++ data/resolvers.js
@@ -10,6 +10,9 @@ export const resolvers = {
             })
         });
     },
+    getAllProducts: () => {
+        return Widgets.find({})
+    },
     createProduct: ({ input }) => {
         const newWidget = new Widgets({
             name: input.name, 
